5731711bb56c0d86d64147e8602946cec2794191
suxinde2009 <suxinde2009@126.com>
2016-01-26 20:58:05 +0800

    Init

---
+++ XDLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDLogger
+//
+//  Created by Su XinDe on 16/1/25.
+//  Copyright © 2016年 com.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
